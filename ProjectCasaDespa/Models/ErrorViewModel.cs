dd5300f7b0e9918157280834648992c32b4ab594
JArangali <jasperlindell.arangali.cics@ust.edu.ph>
2024-01-06 14:03:15 +0800

    Initial Front End

---
+++ ProjectCasaDespa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectCasaDespa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
