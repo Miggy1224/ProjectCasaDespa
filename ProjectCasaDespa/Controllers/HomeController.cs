54dca94ffbdf9a84ac9c3c1440f0ab93876e58ee
Mayu-mii <yumeko.jabamimi@gmail.com>
2024-02-09 21:48:25 +0800

    booking & profile

--- ProjectCasaDespa/Controllers/HomeController.cs
+++ ProjectCasaDespa/Controllers/HomeController.cs
@@ -28,6 +28,28 @@ namespace ProjectCasaDespa.Controllers
             return View();
         }
 
+        public IActionResult Profile()
+        {
+   
+            return View();
+        }
+
+        public IActionResult ProfileP()
+        {
+            return View();
+        }
+
+        public IActionResult Receipt()
+        {
+            return View();
+        }
+
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
